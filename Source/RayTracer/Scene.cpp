4b3c3757eb67dc9e291456cad20c80e734a58dd2
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2023-11-29 14:31:01 -0700

    Camera and scene

---
+++ Source/RayTracer/Scene.cpp
@@ -0,0 +1,41 @@
+#include "Scene.h"
+#include "Canvas.h"
+#include "MathUtils.h"
+
+void Scene::Render(Canvas& canvas)
+{
+	// cast ray for each point (pixel) on the canvas
+	for (int y = 0; y < canvas.GetSize().y; y++)
+	{
+		for (int x = 0; x < canvas.GetSize().x; x++)
+		{
+			// create vec2 pixel from canvas x,y
+			glm::vec2 pixel = glm::vec2{ x, y };
+			// get normalized (0 - 1) point coordinates from pixel
+			glm::vec2 point = pixel / canvas.GetSize();
+			// flip y
+			point.y = 1.0f - point.y;
+
+			// create ray from camera
+			ray_t ray = m_camera->GetRay(point);
+
+			// cast ray into scene
+			// set color value from trace
+			color3_t color = Trace(ray);
+
+			// draw color to canvas point (pixel)
+			canvas.DrawPoint(pixel, color4_t(color, 1));
+		}
+	}
+}
+
+color3_t Scene::Trace(const ray_t& ray)
+{
+	glm::vec3 direction = glm::normalize(ray.direction);
+
+	// set scene sky color
+	float t = (direction.y + 1) * 0.5f; // direction.y (-1 <-> 1) => (0 <-> 1)
+	color3_t color = lerp(m_topColor, m_bottomColor, t);//<lerp between bottom and top color using t>
+
+	return color;
+}
--- Source/RayTracer/main.cpp
+++ Source/RayTracer/main.cpp
@@ -2,19 +2,26 @@
 #include "Color.h"
 #include "Random.h"
 #include "Canvas.h"
+#include "Camera.h"
+#include "Scene.h"
 #include "Renderer.h"
 
 int main(int, char**) {
 	std::cout << "Hello World!" << std::endl;
 
+	seedRandom((int)time(nullptr));
+	
 	Renderer renderer;
 	renderer.Initialize();
 	renderer.CreateWindow("piss window", 400, 300);
 
 	Canvas canvas(400, 300, renderer);
 
-	seedRandom((int)time(nullptr));
+	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0, 1 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 70.0f, aspectRatio);
 
+	Scene scene; // sky color could be set with the top and bottom color
+	scene.SetCamera(camera);
 
 	bool quit = false;
 	while (!quit) {
@@ -24,9 +31,21 @@ int main(int, char**) {
 		case SDL_QUIT:
 			quit = true;
 			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
 		}
 
 		canvas.Clear({ 0, 0, 0, 1 });
+		scene.Render(canvas);
+		//canvas.Update();
+
+		//canvas.Clear({ 0, 0, 0, 1 });
 		for (int i = 0; i < 1000; i++) canvas.DrawPoint({ random(0, canvas.GetSize().x),random(0, canvas.GetSize().y) }, { random(0,1), random(0,1), random(0,1), 1 });
 		canvas.Update();
 
